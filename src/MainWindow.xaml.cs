0118bcbfc4dd70ae8bbe7f0dd6151e5de091b854
Gimberk <james_f_iii@icloud.com>
2026-07-05 23:11:42 -0400

    Initial Layout

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace LSR
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("HELLO WORLD");
+        }
     }
 }
